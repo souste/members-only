02423cb0f89e683e1ec0ca81523e204ceb84bc8e
Stephen <stephensouness@outlook.com>
2025-03-06 14:29:14 +0000

    Pool added, and routes, controller, view set up applied

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
+const authRoutes = require("./routes/authRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.send("Hello Authentication World!"));
+// app.use("/auth", authRoutes);
+// app.use("/messages", messageRoutes);
+// check if this should just be /
+
+const messages = "This is just a placeholder so far!";
+
+app.get("/", (req, res) => res.render("index", { messages }));
+// need to replace this
 
 const PORT = process.env.PORT || 3000;
 
