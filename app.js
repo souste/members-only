0ef399e47d919e3b5b23695b50690f099636ba41
Stephen <stephensouness@outlook.com>
2025-03-06 17:33:02 +0000

    passport functionality installed and added

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const app = express();
 const path = require("node:path");
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
 
 const authRoutes = require("./routes/authRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 // app.use("/auth", authRoutes);
 // app.use("/messages", messageRoutes);
 // check if this should just be /
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
 
 const messages = "This is just a placeholder so far!";
 
