9b31dfc3409d456fd1a270082b4261213893aad6
Stephen <stephensouness@outlook.com>
2025-03-10 13:21:26 +0000

    all messages now rendered on home page

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,13 +1,14 @@
 const pool = require("../db/pool");
 
-const messages = "This is just a placeholder so far!";
-
-const getAllMessages = (req, res) => {
+const getAllMessages = async (req, res) => {
   try {
-    // const messages = await pool.query("SELECT * FROM messages");
-    // res.render("index", { messages: messages.rows, user: req.user });
-    res.render("index", { user: req.user, messages });
+    const result = await pool.query(
+      "SELECT messages.id, messages.title, messages.content, messages.timestamp, users.username FROM messages JOIN users on messages.user_id = users.id ORDER BY messages.timestamp DESC"
+    );
+
+    res.render("index", { messages: result.rows, user: req.user });
   } catch (err) {
+    console.error(err);
     res.status(500).send("Error fetching messages");
   }
 };
@@ -19,7 +20,7 @@ const createMessageGet = (req, res) => {
 const createMessagePost = async (req, res) => {
   try {
     const { title, content } = req.body;
-    const result = await pool.query(
+    await pool.query(
       `INSERT INTO messages (title, content, user_id)
   VALUES ($1, $2, $3)`,
       [title, content, req.user.id]
